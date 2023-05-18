8056eff75544b428eb6e4dbc217e60d83a3b3307
Simon Kunz <sku@mtomady.com>
2023-05-18 17:38:39 +0200

    Add sorting logic & test

--- lib/job_processor_web/controllers/job_controller.ex
+++ lib/job_processor_web/controllers/job_controller.ex
@@ -1,9 +1,70 @@
 defmodule JobProcessorWeb.JobController do
   use JobProcessorWeb, :controller
 
-  def sort(conn, _params) do
-    require IEx
-    IEx.pry
-    json(conn, %{"message" => "Hello world!"})
+  def sort(conn, params) do
+    sorted_tasks =
+      build_tasks_graph(params["tasks"])
+      |> bfs()
+
+    json(conn, sorted_tasks_json(hash_from_tasks(params["tasks"]), sorted_tasks))
+  end
+
+  def bash(conn, params) do
+    sorted_tasks =
+      build_tasks_graph(params["tasks"])
+      |> bfs()
+
+    bash =
+      Enum.reduce(sorted_tasks, "", fn task, acc ->
+        acc <> task <> "\n"
+      end)
+
+    json(conn, %{bash: bash})
+  end
+
+  defp hash_from_tasks(tasks) do
+    Enum.reduce(tasks, %{}, fn task, acc ->
+      Map.put(acc, task["name"], task)
+    end)
+  end
+
+  defp bfs(graph) do
+    Graph.Reducers.Bfs.map(graph, fn v -> v end)
+  end
+
+  defp sorted_tasks_json(tasks_hash, [task_name | task_names]) do
+    task = Map.get(tasks_hash, task_name)
+    task_json = %{name: task["name"], command: task["command"]}
+    [task_json | sorted_tasks_json(tasks_hash, task_names)]
+  end
+
+  defp sorted_tasks_json(tasks_hash, []) do
+    []
+  end
+
+  defp build_tasks_graph(graph \\ Graph.new(type: :directed), [task | tasks]) do
+    graph =
+      graph
+      |> Graph.add_vertex(task["name"])
+      |> add_edges(task["name"], task["requires"])
+
+    build_tasks_graph(graph, tasks)
+  end
+
+  defp build_tasks_graph(graph, []) do
+    graph
+  end
+
+  defp add_edges(graph, task, [requirement | requirements]) do
+    graph = Graph.add_edge(graph, requirement, task)
+    add_edges(graph, task, requirements)
+  end
+
+  defp add_edges(graph, task, []) do
+    graph
+  end
+
+  defp add_edges(graph, task, nil) do
+    graph
   end
 end
--- test/job_processor_web/controllers/job_controller_test.exs
+++ test/job_processor_web/controllers/job_controller_test.exs
@@ -1,32 +1,39 @@
 defmodule JobProcessorWeb.JobControllerTest do
   use JobProcessorWeb.ConnCase
 
-   @attrs %{
-    tasks:
-      [
-        %{
-          name: "task-1",
-          command: "touch /tmp/file1"
-        },
-        %{
-          name: "task-2",
-          command: "cat /tmp/file1",
-          requires: ["task-3"]
-        },
-        %{
-          name: "task-3",
-          command: "echo 'Hello World!' > /tmp/file1",
-          requires: ["task-1"]
-        },
-        %{
-          name: "task-4",
-          command: "rm /tmp/file1",
-          requires: ["task-2","task-3"]
-        }
-      ]
-    }
+  @attrs %{
+    tasks: [
+      %{
+        name: "task-1",
+        command: "touch /tmp/file1"
+      },
+      %{
+        name: "task-2",
+        command: "cat /tmp/file1",
+        requires: ["task-3"]
+      },
+      %{
+        name: "task-3",
+        command: "echo 'Hello World!' > /tmp/file1",
+        requires: ["task-1"]
+      },
+      %{
+        name: "task-4",
+        command: "rm /tmp/file1",
+        requires: ["task-2", "task-3"]
+      }
+    ]
+  }
+
+  @expected_response [
+    %{"command" => "touch /tmp/file1", "name" => "task-1"},
+    %{"command" => "echo 'Hello World!' > /tmp/file1", "name" => "task-3"},
+    %{"command" => "cat /tmp/file1", "name" => "task-2"},
+    %{"command" => "rm /tmp/file1", "name" => "task-4"}
+  ]
+
   test "POST /", %{conn: conn} do
     conn = post(conn, ~p"/api/jobs", @attrs)
-    assert json_response(conn, 200) =~ {}
+    assert @expected_response = json_response(conn, 200)
   end
 end
